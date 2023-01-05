31445b3db77852ae225b0ce340c4b8406cfa0f7c
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2023-01-05 15:33:25 +0100

    commit 2.2

--- Pilotage_robot_distance/Pilotage_robot_distance.ino
+++ Pilotage_robot_distance/Pilotage_robot_distance.ino
@@ -1,14 +1,14 @@
 #include <Ethernet.h> // Inclut la bibliothèque Ethernet
 
-const int avancerPin = 5; // Broche utilisée pour envoyer la commande "avancer"
-const int gauchePin = 6; // Broche utilisée pour envoyer la commande "tourner à gauche"
-const int droitePin = 7; // Broche utilisée pour envoyer la commande "tourner à droite"
-const int reculerPin = 8; // Broche utilisée pour envoyer la commande "reculer"
+const int avancerPin = 1; // Broche utilisée pour envoyer la commande "avancer"
+const int gauchePin = 3; // Broche utilisée pour envoyer la commande "tourner à gauche"
+const int droitePin = 5; // Broche utilisée pour envoyer la commande "tourner à droite"
+const int reculerPin = 9; // Broche utilisée pour envoyer la commande "reculer"
 
-const int led1Pin = 9; // Broche de la LED 1
-const int led2Pin = 10; // Broche de la LED 2
-const int led3Pin = 11; // Broche de la LED 3
-const int led4Pin = 12; // Broche de la LED 4
+const int led1Pin = 6; // Broche de la LED 1
+const int led2Pin = 8; // Broche de la LED 2
+const int led3Pin = 2; // Broche de la LED 3
+const int led4Pin = 3; // Broche de la LED 4
 
 bool led1Etat = LOW; // Etat de la LED 1 (éteint par défaut)
 bool led2Etat = LOW; // Etat de la LED 2 (éteint par défaut)
@@ -16,7 +16,7 @@ bool led3Etat = LOW; // Etat de la LED 3 (éteint par défaut)
 bool led4Etat = LOW; // Etat de la LED 4 (éteint par défaut)
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // Adresse MAC de l'Arduino
-byte ip[] = { ..., ..., ..., ... }; // Adresse IP de l'Arduino
+byte ip[] = { 192, 168, 65, 71 }; // Adresse IP 
 
 EthernetServer server(80); // Crée un serveur TCP sur le port 80
 
@@ -32,19 +32,27 @@ void setup() {
   pinMode(led4Pin, OUTPUT);
   Ethernet.begin(mac, ip);
   server.begin();
+
+
+  Serial.begin(9600);
 }
 
 void loop() {
+  
   // Ecoute les connexions entrantes et traite les commandes reçues
   EthernetClient client = server.available();
+  Serial.println(client);
   if (client) { // Si une connexion est disponible
     String commande = ""; // Crée une chaîne vide pour stocker la commande
     while (client.connected()) { // Tant que le client est connecté
       if (client.available()) { // Si des données sont disponibles à lire
         char c = client.read(); // Lit le caractère suivant
         commande += c; // Ajoute le caractère à la chaîne
+        Serial.println("C'est bon");
       }
     }
+
+    Serial.println(commande);
 
     // Compare la chaîne à différentes valeurs pour savoir quelle commande a été envoyée
     if (commande == "avancer") {
@@ -80,6 +88,12 @@ void loop() {
     digitalWrite(led3Pin, led3Etat);
     digitalWrite(led4Pin, led4Etat);
 
+    
+
     client.stop(); // Ferme la connexion avec le client
+  }
+  else
+  {
+    Serial.println("Pas Bon");
   }
 }
