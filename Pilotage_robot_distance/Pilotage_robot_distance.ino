49c78ed68b45cc4ede3ed48b051f08bfbeb1dca0
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2023-01-20 11:32:27 +0100

    commit 2.7.2

--- Pilotage_robot_distance/Pilotage_robot_distance.ino
+++ Pilotage_robot_distance/Pilotage_robot_distance.ino
@@ -5,11 +5,6 @@ const int led2Pin = 3; // Broche de la LED 2
 const int led3Pin = 4; // Broche de la LED 3
 const int led4Pin = 8; // Broche de la LED 4
 
-bool led1Etat = LOW; // Etat de la LED 1 (éteint par défaut)
-bool led2Etat = LOW; // Etat de la LED 2 (éteint par défaut)
-bool led3Etat = LOW; // Etat de la LED 3 (éteint par défaut)
-bool led4Etat = LOW; // Etat de la LED 4 (éteint par défaut)
-
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // Adresse MAC de l'Arduino
 IPAddress ip(192,168,65,71); // Adresse IP 
 
@@ -47,39 +42,33 @@ void loop() {
 
     if (commande == "avancer") {
       
-      led1Etat = HIGH; // Allume la LED 1
-      led2Etat = HIGH; // Allume la LED 2
-      led3Etat = LOW; // Eteint la LED 3
-      led4Etat = LOW; // Eteint la Led 4
+      digitalWrite(led1Pin, HIGH);
+      digitalWrite(led2Pin, HIGH);
+      digitalWrite(led3Pin, LOW);
+      digitalWrite(led4Pin, LOW);
 
      } else if (commande == "gauche") {
-      
-      led1Etat = LOW; // Eteint la LED 1
-      led2Etat = LOW; // Eteint la LED 2
-      led3Etat = LOW; // Allume la LED 3
-      led4Etat = HIGH; // Eteint la LED 4
+
+      digitalWrite(led1Pin, LOW);
+      digitalWrite(led2Pin, LOW);
+      digitalWrite(led3Pin, LOW);
+      digitalWrite(led4Pin, HIGH);
     } else if (commande == "droite") {
+
+      digitalWrite(led1Pin, LOW);
+      digitalWrite(led2Pin, LOW);
+      digitalWrite(led3Pin, HIGH);
+      digitalWrite(led4Pin, LOW);
       
-      led1Etat = LOW; // Eteint la LED 1
-      led2Etat = LOW; // Eteint la LED 2
-      led3Etat = HIGH; // Eteint la LED 3
-      led4Etat = LOW; // Allume la LED 4
     } else if (commande == "reculer") {
-     
-      led1Etat = LOW; // Eteint la LED 1
-      led2Etat = LOW; // Eteint la LED 2
-      led3Etat = HIGH; // Allume la LED 3
-      led4Etat = HIGH; // Allume la LED 4
+
+      digitalWrite(led1Pin, LOW);
+      digitalWrite(led2Pin, LOW);
+      digitalWrite(led3Pin, HIGH);
+      digitalWrite(led4Pin, HIGH);
     }
 
-    // Met à jour l'état des LED
-    digitalWrite(led1Pin, led1Etat);
-    digitalWrite(led2Pin, led2Etat);
-    digitalWrite(led3Pin, led3Etat);
-    digitalWrite(led4Pin, led4Etat);
 
-    
-    delayMicroseconds(1);
     client.stop(); // Ferme la connexion avec le client
    }
 
